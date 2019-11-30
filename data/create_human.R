70e09ced7d947dd3a033e5abc8a28b0f5a7f5557
valeriacaras <57185850+valeriacaras@users.noreply.github.com>
2019-11-30 12:09:38 +0300

    exercise_5

--- data/create_human.R
+++ data/create_human.R
@@ -53,4 +53,59 @@ str(human)
 write.csv(human, file = "data/human.csv", row.names = FALSE)
 read.csv( file="data/human.csv")
 
-         
+## Data wrangling week 5_Caras Valeria_27.11  
+human <- read.table("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/human1.txt", sep  =",", header = T)
+str(human)
+dim(human)
+tail(human)
+names(human)
+summary(human)
+#This data has 195 obs. of  19 variables and includes different variables on development in different countries.
+# access the stringr package
+library(stringr)
+# look at the structure of the GNI column in 'human'
+str(human$GNI)
+# remove the commas from GNI and print out a numeric version of it
+human$GNI<- str_replace(human$GNI, pattern=",", replace ="")%>%as.numeric
+
+
+library(dplyr)
+# columns to keep
+keep <- c("Country", "Edu2.FM", "Labo.FM", "Life.Exp", "Edu.Exp", "GNI", "Mat.Mor", "Ado.Birth", "Parli.F")
+
+# select the 'keep' columns
+human <- select(human, one_of(keep))
+# print out a completeness indicator of the 'human' data
+complete.cases(human)
+# ?complete.cases()
+# print out the data along with a completeness indicator as the last column
+data.frame(human[-1], comp = complete.cases(human))
+
+# filter out all rows with NA values
+human_ <- filter(human, complete.cases(human))
+human_
+dim(human_)
+# look at the last 10 observations of human
+
+tail(human_, n=10L)
+
+# define the last indice we want to keep
+last <- nrow(human_) - 7
+
+# choose everything until the last 7 observations
+newhuman <- human_[1:last, ]
+
+# add countries as rownames
+rownames(newhuman) <- newhuman$Country
+newhuman <- select(newhuman, -Country)
+# now data has 155 observations and 8 variables
+dim(newhuman)
+head(newhuman)
+tail(newhuman)
+
+# save including row names
+
+write.csv(newhuman, file = "data/newhuman.csv", row.names = TRUE)
+read.csv( file="data/newhuman.csv", row.names = 1)
+
+
